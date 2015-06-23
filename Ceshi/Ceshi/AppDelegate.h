1ca5cf2b9c5b6f32aaaa385289416830b0347da9
llxjsw <llxwwdhxy2@163.com>
2015-06-23 09:46:02 +0800

    no message

---
+++ Ceshi/Ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ceshi
+//
+//  Created by 李立兴 on 15/6/23.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ceshi/Ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ceshi
+//
+//  Created by 李立兴 on 15/6/23.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
